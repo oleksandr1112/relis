933178265b6e25b63e44180c448ec38b8d4e7fc2
Oleksandr <sasha.chernetskyi09@gmail.com>
2025-04-27 22:04:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ send_btn.addEventListener('click', function(){
         document.querySelector('.your_coment_text').value = ''
     }
 })
+
+class Goods {
+    constructor(name, spice, img){
+        this.name = name
+        this.spice = spice
+        this.img = img
+
+    }
+}
+let goods1 = new Goods ('ДИВАН', '100грн', 'goods1.png' )
+let goods2 = new Goods ("М'ЯКЕ КРІСЛО ", '100грн', 'goods2.png' )
+let goods3 = new Goods ("М'ЯКЕ КРІСЛО", '100грн', 'goods3.png' )
+let goods4 = new Goods ('ДИВАН', '100грн', 'goods4.png' )
+let btn_buy1 = document.querySelector('.btn_buy1')
+let btn_buy2 = document.querySelector('.btn_buy2')
+let btn_buy3 = document.querySelector('.btn_buy3')
+let btn_buy4 = document.querySelector('.btn_buy4')
+btn_buy1.addEventListener('click', function(){
+    btn_buy1.style.background = 'white'
+    btn_buy1.style.color = '#9AA6B2'
+    btn_buy1.style.border = '1px solid #9AA6B2'
+})
